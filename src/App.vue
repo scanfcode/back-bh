207e7d356ddc4f69d1b50a9c2917136bf2de1803
launit <claytoncsj520@gmail.com>
2019-08-23 10:18:18 -0300

    front

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,143 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav role="navigation">
+      <div id="menuToggle">
+        <input type="checkbox" />
+
+        <span></span>
+        <span></span>
+        <span></span>
+
+        <ul id="menu">
+          <a href="#home">
+            <li>
+              <router-link to="/">Home</router-link>
+            </li>
+          </a>
+          <a href="#about">
+            <li>Sobre</li>
+          </a>
+          <a href="#service">
+            <li>Serviços</li>
+          </a>
+          <a href="#portifolio">
+            <li>Portifolio</li>
+          </a>
+          <a href="#skills">
+            <li>Habilidades</li>
+          </a>
+          <a href="#contact">
+            <li>Contatos</li>
+          </a>
+        </ul>
+      </div>
+    </nav>
+    <div class="navbar">
+      <img src="./static/img/logo.png" alt />
+      <div class="navigation">
+        <ul>
+          <li>
+            <a href>Modelagem</a>
+          </li>
+          <li>
+            <a href>SQL</a>
+          </li>
+          <li>
+            <a href>Nosql</a>
+          </li>
+          <li>
+            <a href>Nosql</a>
+          </li>
+        </ul>
+      </div>
+      <div class="form">
+        <form action>
+          <input type="search" name id placeholder="Buscar..." />
+        </form>
+      </div>
     </div>
-    <router-view/>
+    <router-view></router-view>
+
+    <div id="footer">
+      <footer>
+        <!-- <div class="socias-icons"> -->
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/clayton.silvadejesus.1" target="_blank">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/Launitweb" target="_blank">
+              <i class="fab fa-twitter-square"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#0">
+              <i class="fab fa-whatsapp-square"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/claytoncsj" target="_blank">
+              <i class="fab fa-github-square"></i>
+            </a>
+          </li>
+        </ul>
+        <a href="https://www.facebook.com/scanfcode/" target="_blank">www.scanfcode.com.br</a>
+        <p>&copy;Todos direitos reservados</p>
+        <!-- </div> -->
+      </footer>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: "app"
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+nav {
+  margin: 0;
 }
-#nav {
+.navbar {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
   padding: 30px;
+  background-color: #000000;
+  align-items: center;
+}
+.navbar img {
+  width: 120px;
+}
+.navbar ul {
+  display: flex;
+}
+.navbar li {
+  padding: 0 20px;
+}
+#footer {
+  margin: 0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+/*  */
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+form {
+  padding: 0 30px;
+}
+input {
+  background-color: rgba(255, 255, 255, 0.527);
+  color: 000;
+  height: 45px;
+  border: none;
+  outline: none;
+  border-radius: 50px;
+  padding: 0px 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import style from "./static/css/style.css";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  style,
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
